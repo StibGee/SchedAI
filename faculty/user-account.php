1abab1fedc08cfa8e26f07106976248fd766d81e
Stib <steve1020.garciano@gmail.com>
2024-10-30 02:53:23 +0800

    another push

--- faculty/user-account.php
+++ faculty/user-account.php
@@ -30,14 +30,14 @@
     <div class="container-fluid ">
         <div class="button col-4 col-sm-4">
             <button class="btn btn-outline-secondary" type="button" data-bs-toggle="collapse"
-                data-bs-target="#collapseWidthExample" aria-expanded="true" aria-controls="collapseWidthExample"
-                style="margin-right: 10px; padding: 0px 5px 0px 5px;" id="sidebartoggle" onclick="changeclass()">
-                <i class="bi bi-arrows-expand-vertical"></i>
+                    data-bs-target="#collapseWidthExample" aria-expanded="true" aria-controls="collapseWidthExample"
+                    style="margin-right: 10px; padding: 0px 5px 0px 5px;" id="sidebartoggle" onclick="changeclass()">
+                    <i class="bi bi-list"></i>
             </button>
             <button class="btn btn-outline-secondary" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
-                style="margin-right: 10px; padding: 2px 6px 2px 6px;" id="sidebarshow">
-                <i class="bi bi-arrow-bar-right"></i>
+                    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
+                    style="margin-right: 10px; padding: 2px 6px 2px 6px;" id="sidebarshow">
+                    <i class="bi bi-list"></i>
             </button>
         </div>
 
@@ -141,12 +141,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
 integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
 
